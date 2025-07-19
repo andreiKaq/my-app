4088296f8069850b702b24c0005e7a36c616cb90
dronfury <andreikagu03@gmail.com>
2025-07-19 14:53:18 +0300

    homework-3.0 - Registation form

--- src/AuthForm.jsx
+++ src/AuthForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import AuthControl from './AuthControl';
 import { signIn } from './services/api/Auth';
+import { signUp } from './services/api/Auth';
 
 const actions = {
     SignIn: 'signIn',
@@ -9,21 +10,30 @@ const actions = {
 
 }
 
-function AuthForm({formRef, setAuthData, handleClose}) {
+function AuthForm({ formRef, setAuthData, handleClose }) {
     const [action, setAction] = React.useState(actions.SignIn)
 
     const handleOnSelect = (action) => setAction(action)
-    
+
     const handleSubmit = async event => {
         event.preventDefault()
-        
-        const username = event.target.username.value
-        // const email = event.target.email?.value
-        const password = event.target.password.value        
-        
-        const { token } = await signIn(username, password)
 
-        setAuthData(authData => ({...authData, jwt: token}))
+        const username = event.target.username.value
+        const password = event.target.password.value
+
+        if (action === actions.SignIn) {
+            const { token } = await signIn(username, password)
+            setAuthData(authData => ({ ...authData, jwt: token }))
+        } else if (action === actions.SignUp) {
+            const email = event.target.email?.value
+            const user = await signUp(username, email, password)
+            setAuthData(authData => ({...authData, user}))
+            console.log(user)
+        }
+
+
+
+
 
         handleClose()
     }
@@ -33,13 +43,13 @@ function AuthForm({formRef, setAuthData, handleClose}) {
             <AuthControl action={action} actions={actions} handleOnSelect={handleOnSelect} />
             <Form.Group className="mb-3" controlId="formBasicUserName">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" name='username' defaultValue='hopkins'/>
+                <Form.Control type="text" placeholder="Enter username" name='username' defaultValue='hopkins' />
             </Form.Group>
 
             {action === actions.SignUp && (
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="email" name='email'/>
+                    <Form.Control type="email" placeholder="email" name='email' />
                 </Form.Group>
 
             )}
@@ -48,7 +58,7 @@ function AuthForm({formRef, setAuthData, handleClose}) {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" name='password' defaultValue='William56$hj'/>
+                <Form.Control type="password" placeholder="Password" name='password' defaultValue='William56$hj' />
             </Form.Group>
         </Form>
     );
